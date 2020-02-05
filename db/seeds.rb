20325e3ca5ecc9d14bbf9a3f8d74c75bc772823a
Julianna Boyd <julianna.boyd91@gmail.com>
2020-02-05 11:39:13 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+song1 = Song.create(name: "song 1", artist_id: artist2.id, genre_id: genre1.id)
+song2 = Song.create(name: "song 2", artist_id: artist3.id, genre_id: genre2.id)
+song3 = Song.create(name: "song 3", artist_id: artist2.id, genre_id: genre3.id)
+
+genre1 = Genre.create(name: "genre 1")
+genre2 = Genre.create(name: "genre 2")
+genre3 = Genre.create(name: "genre 3")
+
+artist1 = Artist.create(name: "artist 1", bio: "whatever")
+artist2 = Artist.create(name: "artist 2", bio: "whatever")
+artist3 = Artist.create(name: "artist 3", bio: "whatever")
